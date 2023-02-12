9628190fe592bd833d6f1c2c9f67ad983efeb79a
Mateusz Szostek <codeartist.net@gmail.com>
2023-02-12 22:45:45 +0100

    package npm update

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
+import SEO from "../components/SEO";
+import { graphql } from "gatsby";
 
 const NotFoundPage = () => (
   <Layout>
@@ -9,3 +11,47 @@ const NotFoundPage = () => (
 );
 
 export default NotFoundPage;
+
+export const query = graphql`
+  query indexPageQuery {
+    allFile(filter: { name: { eq: "ca-logo" }, extension: { eq: "png" } }) {
+      edges {
+        node {
+          publicURL
+        }
+      }
+    }
+  }
+`;
+
+export const Head = (props) => {
+  return (
+    <SEO
+      title="Code Artist - Software Development"
+      description="We turn coffee into code and create websites faster than the speed of light. Need a new website? We've got you covered... or at least, covered in lines of code."
+      keywords="Software development, Web development, Mobile app development, Custom software solutions, Full-stack development, Software architecture"
+      canonicalUrl={process.env.DOMAIN}
+      ogType="website"
+      ogUrl={process.env.DOMAIN}
+      ogTitle="Code Artist - Software Development"
+      ogDescription="We turn coffee into code and create websites faster than the speed of light. Need a new website? We've got you covered... or at least, covered in lines of code."
+      ogImage={`${process.env.DOMAIN}${props?.data?.allFile?.edges[0].node?.publicURL}`}
+      ogLocale="en_gb"
+      //ogVideo,
+      //ogVideoUrl,
+      //ogVideoSecureUrl,
+      //ogVideoType,
+      //ogVideoWidth,
+      //ogVideoHeight,
+      //ogImageUrl,
+      //ogImageSecureUrl,
+      //ogImageType,
+      //ogImageWidth,
+      //ogImageHeight,
+      //fbAppId,
+      //twCard,
+      //twSite,
+      //twCreator,
+    />
+  );
+};
--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout";
 import SimpleTemplate from "../modules/SimpleTemplate";
+import SEO from "../components/SEO";
+import { graphql } from "gatsby";
 
 const ArticleTemplate = (props) => {
   console.log(props);
@@ -12,3 +14,49 @@ const ArticleTemplate = (props) => {
 };
 
 export default ArticleTemplate;
+
+export const query = graphql`
+  query indexPageQuery {
+    allFile(filter: { name: { eq: "ca-logo" }, extension: { eq: "png" } }) {
+      edges {
+        node {
+          publicURL
+        }
+      }
+    }
+  }
+`;
+
+export const Head = (props) => {
+  console.log(props);
+  console.log(process.env.DOMAIN);
+  return (
+    <SEO
+      title="Code Artist - Software Development"
+      description="We turn coffee into code and create websites faster than the speed of light. Need a new website? We've got you covered... or at least, covered in lines of code."
+      keywords="Software development, Web development, Mobile app development, Custom software solutions, Full-stack development, Software architecture"
+      canonicalUrl={process.env.DOMAIN}
+      ogType="website"
+      ogUrl={process.env.DOMAIN}
+      ogTitle="Code Artist - Software Development"
+      ogDescription="We turn coffee into code and create websites faster than the speed of light. Need a new website? We've got you covered... or at least, covered in lines of code."
+      ogImage={`${process.env.DOMAIN}${props?.data?.allFile?.edges[0].node?.publicURL}`}
+      ogLocale="en_gb"
+      //ogVideo,
+      //ogVideoUrl,
+      //ogVideoSecureUrl,
+      //ogVideoType,
+      //ogVideoWidth,
+      //ogVideoHeight,
+      //ogImageUrl,
+      //ogImageSecureUrl,
+      //ogImageType,
+      //ogImageWidth,
+      //ogImageHeight,
+      //fbAppId,
+      //twCard,
+      //twSite,
+      //twCreator,
+    />
+  );
+};
